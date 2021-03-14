2a99d3d0487f3604481aa31419ec0d1cadaab292
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-14 14:37:14 +0530

    [Rinkesh] : add Calculating Wage Till a condition Meet

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,11 +4,14 @@ public class EmployeeWage {
 	public static final int Is_Part_Time = 2;
 	public static final int Wage_Per_Hour = 20;
 	public static final int Num_Of_Woking_Days = 20;
+	public static final int Max_Hrs_In_Month = 100;
 	public static void main(String[] args) {
 	// Variables
-	int empHrs = 0, empWage = 0, totalEmpWage = 0;
+	int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
 		// Computation
-		for (int day = 0; day <  Num_Of_Woking_Days; day++) {
+		while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Woking_Days) {
+		int empHrs = 0;
+		totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random() * 10) %3;
 		switch (empCheck) {
 			case Is_Full_Time:
@@ -19,8 +22,9 @@ public class EmployeeWage {
 				break;
 			default:
 				empHrs = 0;
-		}
-			empWage = empHrs * Wage_Per_Hour;
+			}
+			totalEmpHrs += empHrs;
+			int empWage = empHrs * Wage_Per_Hour;
 			totalEmpWage += empWage;
 			System.out.println("Employee Wage: " + empWage);
 		}
