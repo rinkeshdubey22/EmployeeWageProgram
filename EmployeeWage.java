98334065d5c97524fefec2edaf84cf6c8f540532
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-14 12:09:43 +0530

    [Rinkesh] : add Add Part time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 	// Constraints
 	int Is_Full_Time = 1;
+	int Is_Part_Time = 2;
 	int Wage_Per_Hour = 20;
 	// Variables
 	int empHrs = 0;
 	int empWage = 0;
 		// Computation
-		double empCheck = Math.floor(Math.random() * 10) %2;
+		double empCheck = Math.floor(Math.random() * 10) %3;
 		if (empCheck == Is_Full_Time) {
 			 empHrs = 8;
+		}
+		else if (empCheck == Is_Part_Time) {
+			empHrs = 4;
 		}
 		else {
 			empHrs = 0;
